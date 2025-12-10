9a80df26969bda9a9181a4c8001f41a4a9b09791
Farooq Olanrewaju <ofarooqdev@gmail.com>
2025-12-10 04:19:20 +0300

    chore: initial setup of layouts

---
+++ src/app/new-user-onboarding/_layout.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function UserOnboardingLayout() {
+  return (
+    <View>
+      <Text>New User Onboarding Layout</Text>
+    </View>
+  );
+}
+
+export default UserOnboardingLayout;
