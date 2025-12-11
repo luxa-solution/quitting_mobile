15f24cc2990336341ce8e5569d0497b1ca45ef25
Farooq <ofarooqdev@gmail.com>
2025-12-11 02:53:31 +0100

    Develop (#49)
    * chore: setting up files for onboarding and auth (#19)
    
    * Hotfix/pr guard (#46)
    
    * hotfix(chore): error from pr-guard - wrong issue permission
    
    * hotfix(chore): add a checkout to develop
    
    * Hotfix/propagation reorder (#48)
    
    * hotfix(chore): error from pr-guard - wrong issue permission (#44)
    
    * hotfix(chore): error from pr-guard - wrong issue permission
    
    * hotfix(chore): add a checkout to develop
    
    * hotfix(chore): reorder propagation workflow and guidelines

--- src/app/auth/_layout.tsx
+++ src/app/auth/_layout.tsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native';
+import { Stack } from 'expo-router';
+import { useUnistyles } from 'react-native-unistyles';
 
 function AuthLayout() {
+  const { theme } = useUnistyles();
+
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.colors.background,
+        },
+        headerTitleStyle: {
+          color: theme.colors.typography,
+        },
+        animation: 'none',
+      }}
+      initialRouteName="login">
+      <Stack.Screen name="login" />
+      <Stack.Screen name="signup" />
+      <Stack.Screen name="forgot-password" />
+    </Stack>
   );
 }
 
