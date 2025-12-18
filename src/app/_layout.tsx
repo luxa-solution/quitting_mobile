d318be4acad9a9723f5ec0fcb462d7c6ef9ee00e
Farooq <ofarooqdev@gmail.com>
2025-12-18 22:53:13 +0100

    fix(splash-screen): prevent autohide until hydrated (#68)
    * feature(splash): install expo-splash-screen
    
    * fix(splash-screen): prevent autohide until hydrated

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,11 @@
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useUnistyles } from 'react-native-unistyles';
 
 import '@/translation';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const { theme } = useUnistyles();
@@ -20,11 +23,12 @@ export default function RootLayout() {
             color: theme.colors.typography,
           },
           headerTintColor: theme.colors.typography,
+          headerShown: false,
         }}>
-        <Stack.Screen name="tabs" options={{ headerShown: false }} />
-        <Stack.Screen name="auth" options={{ headerShown: false }} />
-        <Stack.Screen name="new-app-onboarding" options={{ headerShown: false }} />
-        <Stack.Screen name="new-user-onboarding" options={{ headerShown: false }} />
+        <Stack.Screen name="tabs" />
+        <Stack.Screen name="auth" />
+        <Stack.Screen name="new-app-onboarding" />
+        <Stack.Screen name="new-user-onboarding" />
       </Stack>
     </>
   );
